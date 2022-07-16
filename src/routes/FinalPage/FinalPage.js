f9ea34e79777c690224e439aea476c2c25e8ca5f
Kris G <kgiesk@gmail.com>
2022-07-16 15:12:47 -0500

    continuing puzzle 3

--- src/routes/FinalPage/FinalPage.js
+++ src/routes/FinalPage/FinalPage.js
@@ -2,30 +2,27 @@ import { Typography } from "@mui/material";
 import React from "react";
 import Image from "mui-image";
 import cake from "./cake.gif";
-import { Grid } from "@mui/material";
+import { Box } from "@mui/material";
 
 export default function FinalPage() {
   return (
-    <>
-      <Grid
-        container
-        direction="column"
-        justifyContent="center"
-        alignItems="center"
-      >
-        <Grid item xs={12}         justifyContent="center"
-        alignItems="center">
-          <Image
-            src={cake}
-            width={100}
-            style={{ display: "block" }}
-            alt={"Cake is a lie"}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <Typography variant={"h3"}> You Quit! </Typography>
-        </Grid>
-      </Grid>
-    </>
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      minHeight="100vh"
+      sx={{ flexDirection: "column" }}
+    >
+      <Image
+        showLoading
+        src={cake}
+        width={128}
+        alt={"Cake is a lie"}
+        shift="bottom"
+        distance="100px"
+        shiftDuration={900}
+      />
+      <Typography variant={"h3"}> You Quit! </Typography>
+    </Box>
   );
 }
--- src/routes/Puzzle002/Puzzle002.js
+++ src/routes/Puzzle002/Puzzle002.js
@@ -1,48 +1,44 @@
 import React, { useEffect, useState } from "react";
-import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
+import { Box } from "@mui/material";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import { Link } from "react-router-dom";
 
 export default function Puzzle002() {
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
   const [valid, setValid] = useState(false);
 
   useEffect(() => {
-    if (text === 'Answer') {
+    if (text === "Answer") {
       setValid(true);
-    } else {setValid(false)}
+    } else {
+      setValid(false);
+    }
   }, [text]);
 
   return (
     <Box
-      sx={{
-        "& .MuiTextField-root": { m: 1, width: "25ch" },
-      }}
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      minHeight="100vh"
+      sx={{ flexDirection: "column" }}
     >
-      <Grid
-        container
-        direction="column"
-        justifyContent="center"
-        alignItems="center"
-      >
-        <p>The Answer is Your Clue</p>
-        <TextField
-          autoComplete="off"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-          error={!valid}
-          // label={!valid ? 'Wrong' : ''}
-          // helperText={valid ? 'Incorrect entry.' : 'Yo'}
-          id="SaveName"
-          placeholder="Name?"
-          variant="outlined"
-        />
-        <Button
-        disabled={!valid} component={ Link } to="/puzzle003"
-        >Save & Quit</Button>
-      </Grid>
+      <p>The Answer is Your Clue</p>
+      <TextField
+        autoComplete="off"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        error={!valid}
+        // label={!valid ? 'Wrong' : ''}
+        // helperText={valid ? 'Incorrect entry.' : 'Yo'}
+        id="SaveName"
+        placeholder="Name?"
+        variant="outlined"
+      />
+      <Button disabled={!valid} component={Link} to="/puzzle003">
+        Save & Quit
+      </Button>
     </Box>
   );
 }
