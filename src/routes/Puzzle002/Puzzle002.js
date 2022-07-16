1aa6fae406efb58967a3f0b2b45dd9f493fdb8fb
Kris G <kgiesk@gmail.com>
2022-07-15 22:45:34 -0500

    more work on Puzzle 2

--- src/routes/Puzzle002/Puzzle002.js
+++ src/routes/Puzzle002/Puzzle002.js
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
 import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 
-export default function InputSave() {
+export default function Puzzle002() {
+  const [formIsValid,setFormIsValid] = useState(false);
+
   return (
-    <TextField
-      error
-      id="outlined"
-      label="Error"
-      defaultValue="Enter Save Name"
-    />
+    <Box
+      sx={{
+        "& .MuiTextField-root": { m: 2, width: "25ch" },
+      }}
+    >
+      <Grid
+        container
+        direction="column"
+        justifyContent="flex-end"
+        alignItems="center"
+      >
+        <TextField
+          // error
+          helperText="Enter the correct name."
+          id="SaveName"
+          defaultValue="?"
+        />
+        <Button
+         type="submit" 
+        //  disabled={!formIsValid()}
+         >
+          Save & Quit
+        </Button>
+      </Grid>
+    </Box>
   );
 }
